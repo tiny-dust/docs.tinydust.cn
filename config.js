5b070f179eadd098d9c2a9771ead6f47a22ef6a2
PortWatcher <root@pwhack.me>
2014-08-18 19:48:31 +0800

    change port

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
 
 	// Your site title (format: page_title - site_title)
-	site_title: 'Raneto Docs',
+	site_title: 'LydiaBox Web App 开发文档',
 
 	// The base URL of your site (can use %base_url% in Markdown files)
 	base_url: '',
